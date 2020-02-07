b876026bec1fa610a6822793b1bf03153b983545
Chris <chrishaining@yahoo.co.uk>
2020-02-07 20:06:18 +0000

    fixes link from admin to index

--- quiz/cards/admin.py
+++ quiz/cards/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from cards.models import *
+from django.contrib.admin import sites
 
 # Register your models here.
 admin.site.register(Card)
+sites.AdminSite.site_url = '/cards/'
