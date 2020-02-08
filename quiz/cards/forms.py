4070d520eff654a72147cc1b50ce16a89f245b16
Chris <chrishaining@yahoo.co.uk>
2020-02-08 12:03:33 +0000

    fixed a bug where the form was showing an unwanted field

--- quiz/cards/forms.py
+++ quiz/cards/forms.py
@@ -1,9 +1,7 @@
 from django import forms
-
 from .models import Card
 
 class CardForm(forms.ModelForm):
-    card = forms.ModelChoiceField(Card.objects.all())
     class Meta:
         model = Card
         fields = ('question', 'answer', 'url')
